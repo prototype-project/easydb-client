dfc665fb783a07c68b32949bd4bdf158c9fd9c6d
Daniel Faderski <daniel.faderski@allegro.pl>
2018-12-06 01:20:15 +0100

    base test fix

--- tests/base_test.py
+++ tests/base_test.py
@@ -7,48 +7,65 @@ from aiohttp import web
 from aiohttp.test_utils import AioHTTPTestCase, TestServer
 
 
-class RequestMappingKey:
-    def __init__(self, path: str, method: str = None, request: dict = None, query_params: dict = None):
+class RequestRecord:
+    def __init__(self, path: str, method: str, request_body: dict = None,
+                 query_params: dict = None):
         self.path = path
         self.method = method
-        self.request = request
-        self.query_params = query_params
-        if not self.query_params:
-            self.query_params = {}
+        self.request_body = request_body if request_body else {}
+        self.query_params = query_params if query_params else {}
 
-    def __hash__(self):
-        return hash((self.path, self.method, json.dumps(self.request), tuple(self.query_params)))
 
-    def __eq__(self, other):
-        return self.path == other.path and \
-               self.method == other.method and \
-               self.request == other.request and \
-               tuple(self.query_params) == tuple(other.query_params)
+class ResponseRecord:
+    def __init__(self, status: int = 200, response_body: dict = None):
+        self.invokaction_count = 0
+        self.status = status
+        self.response_body = response_body if response_body else {}
 
-    def __str__(self):
-        return "RequestMappingKey(path = %s, method = %s, request = %s, query_params = %s)" % \
-               (self.path, self.method, self.request, self.query_params)
-
-    def __repr__(self):
-        return self.__str__()
+    def mark_invocation(self):
+        self.invokaction_count += 1
 
 
 class RequestNotMatchException(Exception):
-    def __init__(self, actual, required):
+    def __init__(self, actual: RequestRecord, closest_match: RequestRecord):
         self.actual = actual
-        self.required = required
-
-    def __str__(self):
-        return 'RequestNotMatchException(actual = %s, required = %s)' % (self.actual, self.required)
-
-    def __repr__(self):
-        return self.__str__()
+        self.closest_match = closest_match
 
 
-class RequestMappingValue:
-    def __init__(self, response_body: dict=None):
-        self.count = 0
-        self.response_body = response_body if response_body else {}
+# /users/ POST req = {} query_params = {} -> status  resp = {}
+
+class RequestMappingRecorder:
+    NUMBER_OF_RECORD_ATTRIBUTES = 4
+    def __init__(self):
+        self.records = []
+
+    def add_record(self, request: RequestRecord, response: ResponseRecord):
+        self.records.append((request, response))
+
+    def find_closest(self, given: RequestRecord):
+        max_match_fields = 0
+        closest = None
+
+        for req, res in self.records_for_route(given.path, given.method):
+            match_fields = 2 # path and method match for sure
+            if not req.request_body or req.request_body == given.request_body:
+                match_fields += 1
+            if not req.query_params or req.query_params == given.query_params:
+                match_fields += 1
+            if match_fields > max_match_fields:
+                closest = req, res
+                max_match_fields = match_fields
+
+        if max_match_fields != self.NUMBER_OF_RECORD_ATTRIBUTES:
+            raise RequestNotMatchException(given, closest[0])
+
+        return closest[1]
+
+    def contains_route(self, path:str, method:str):
+        return len(self.records_for_route(path, method))
+
+    def records_for_route(self, path, method):
+        return list(filter(lambda r: r[0].path == path and r[0].method == method, self.records))
 
 
 class HttpTest(AioHTTPTestCase):
@@ -57,9 +74,7 @@ class HttpTest(AioHTTPTestCase):
     server_url = 'http://localhost:' + str(server_port)
 
     def setUp(self):
-        self.request_mapping = {}
-        self.routes = set()
-
+        self.request_recorder = RequestMappingRecorder()
         self.web_app = web.Application()
         setup_method = getattr(self, 'before_' + self._testMethodName, None)
         if setup_method:
@@ -77,30 +92,33 @@ class HttpTest(AioHTTPTestCase):
         request_body = self.read_file(request_file)
         response_body = self.read_file(response_file)
 
-        self.request_mapping[RequestMappingKey(path, method, request_body, query_params)] = RequestMappingValue(response_body)
-
-        if path not in self.routes:
+        if not self.request_recorder.contains_route(path, method):
             self.web_app.router.add_routes(
-                [route(method, path, handler=self.handler_factory(request_body, status))])
-        self.routes.add(path)
+                [route(method, path, handler=self.handler_factory())])
 
-    def handler_factory(self, request_body: dict, status=200):
+        self.request_recorder.add_record(RequestRecord(path, method, request_body, query_params),
+                                         ResponseRecord(status, response_body))
+
+    def handler_factory(self):
         async def handle(request: Request):
-            if request.can_read_body and await request.json() != request_body:
-                raise RequestNotMatchException(await request.json(), request_body)
+            body = await request.json() if request.can_read_body else None
+            expected_response = self.request_recorder.find_closest(
+                RequestRecord(request.path, request.method, body, dict(request.query)))
+            expected_response.mark_invocation()
 
-            request_mapping = self.request_mapping[RequestMappingKey(request.path, request.method, request_body, dict(request.query))]
-            request_mapping.count += 1
-            return web.Response(body=json.dumps(request_mapping.response_body), headers={'Content-Type': 'application/json'},
-                                status=status)
+            return web.Response(body=json.dumps(expected_response.response_body),
+                                headers={'Content-Type': 'application/json'},
+                                status=expected_response.status)
 
         return handle
 
     def verify(self, path: str, method: str, request_file: str = None, query_params: dict = None):
         request_body = self.read_file(request_file)
-        return self.request_mapping.get(RequestMappingKey(path, method, request_body, query_params), RequestMappingValue()).count
+        return self.request_recorder.find_closest(
+            RequestRecord(path, method, request_body, query_params)).invokaction_count
 
-    def read_file(self, file: str = None):
+    @staticmethod
+    def read_file(file: str = None):
         if file:
             with open(HttpTest.rules_path + '/' + file) as file:
                 return json.load(file)
